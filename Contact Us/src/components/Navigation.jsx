74f849084c6ad832c8d58e42ab6c2cc85314efc7
Ritwik <ritwikhero@gmail.com>
2024-06-06 21:52:35 +0530

    navigation component work started

---
+++ Contact Us/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+const Navigation = () => {
+
+    return (
+    <nav className="container">
+        <div className="logo">
+            <img src="/images/logo.png" alt="do some coding logo" />
+        </div>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+    </nav>
+    )
+}
+
+export default Navigation
